70f230caa2049da0765df86c57b64df6553b746e
krish-nakrani-2002 <nakranikrish5@gmail.com>
2024-12-11 15:10:23 +0530

    add buttons

--- bgchanger/src/App.jsx
+++ bgchanger/src/App.jsx
@@ -5,7 +5,26 @@ function App() {
 
   return (
     <>
-    <div className="w-full h-screen duration-200"></div>
+    <div className="w-full h-screen duration-200" style={{backgroundColor: color}}>
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "red"}}>Red</button>
+
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "orange"}}>orange</button>
+
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "blue"}}>blue</button>
+
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "black"}}>black</button>
+
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "grey"}}>grey</button>
+        </div>
+      </div>
+    </div>
     </>
   )
 }
