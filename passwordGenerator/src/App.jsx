3c7ec774f2c68c593b0168c06cae091f568e268e
krish-nakrani-2002 <nakranikrish5@gmail.com>
2024-12-12 16:38:12 +0530

    add button and input length

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState, useCallback } from 'react'
-import './App.css'
 
 function App() {
   const [length, setLength] = useState(8)
@@ -17,7 +16,7 @@ function App() {
       let char = Math.floor(Math.random() * str.length + 1)
       pass = str.charAt(char)
     }
-    
+
     setPassword(pass)
       
   }, [length, numAllowd, charAllowd, setPassword])
@@ -25,7 +24,32 @@ function App() {
 
   return (
     <>
-    <h1 className='text-4xl text-center text-white'>Password Generator</h1>
+    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-800'>
+      <h1 className='text-white text-center my-3'>Password Generator</h1>
+      <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+        <input 
+        type="text"
+        value={Password}
+        className='outline-none w-full py-1 px-3'
+        placeholder='password'
+        readOnly
+        />
+        <button className='outline-none bg-blue-700 text-white px-3 py-1 shrink-0'>Copy</button>
+      </div>
+      <div className='flex text-sm gap-x-2'>
+        <div className='flex items-center gap-x-1'>
+          <input 
+          type="range"
+          min={6}
+          max={100}
+          value={length}
+          className='cursor-pointer'
+          onChange={(e) => {setLength(e.target.value)}}
+          />
+          <label>Length: {length}</label>
+        </div>
+      </div>
+    </div>
     </>
   )
 }
