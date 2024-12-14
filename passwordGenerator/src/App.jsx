bbf215e51f73d63bb2145bd883702bebc3d1c3e5
krish-nakrani-2002 <nakranikrish5@gmail.com>
2024-12-14 15:58:27 +0530

    set password using useEffect

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react'
+import { useState, useCallback, useEffect } from 'react'
 
 function App() {
   const [length, setLength] = useState(8)
@@ -12,15 +12,18 @@ function App() {
     if (numAllowd) str += "0123456789"
     if (charAllowd) str += "!@#$%^&*()-_=+\|[]{}?`~"
 
-    for (let i = 1; i <= array.length; i++) {
+    for (let i = 1; i <= length; i++) {
       let char = Math.floor(Math.random() * str.length + 1)
-      pass = str.charAt(char)
+      pass += str.charAt(char)
     }
 
     setPassword(pass)
       
   }, [length, numAllowd, charAllowd, setPassword])
 
+  useEffect(() => {
+    passwdGenerator()
+  }, [length, numAllowd, charAllowd, passwdGenerator])
 
   return (
     <>
@@ -36,18 +39,20 @@ function App() {
         />
         <button className='outline-none bg-blue-700 text-white px-3 py-1 shrink-0'>Copy</button>
       </div>
+      
       <div className='flex text-sm gap-x-2'>
         <div className='flex items-center gap-x-1'>
           <input 
           type="range"
           min={6}
-          max={100}
+          max={20}
           value={length}
           className='cursor-pointer'
           onChange={(e) => {setLength(e.target.value)}}
           />
           <label>Length: {length}</label>
         </div>
+
         <div className='flex items-center gap-x-1'>
           <input 
           type="checkbox"
@@ -59,6 +64,7 @@ function App() {
           />
           <label htmlFor="numberInput">Numbers</label>
         </div>
+
         <div className='flex items-center gap-x-1'>
         <input 
           type="checkbox"
