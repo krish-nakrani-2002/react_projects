fe46c13401e3589ee93a0be3759b413c2f53db92
krish-nakrani-2002 <nakranikrish5@gmail.com>
2024-12-17 10:27:54 +0530

    footer and home

--- reactRouter/src/App.jsx
+++ reactRouter/src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './Components/Home/home'
+import Footer from './Components/Footer/footer'
+import Header from './Components/Header/header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1 className='bg-green-600 p-4'>React Router</h1>
+    
     </>
   )
 }
