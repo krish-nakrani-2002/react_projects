551d0c7f6b83b14c76b32d142580f261be093b1f
krish-nakrani-2002 <nakranikrish5@gmail.com>
2024-12-17 10:38:49 +0530

    render home and about

--- reactRouter/src/main.jsx
+++ reactRouter/src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './Components/Home/home.jsx'
+import About from './Components/About/About.jsx'
 
 const router = createBrowserRouter([
   {
@@ -14,6 +15,10 @@ const router = createBrowserRouter([
       {
         path: "",
         element: <Home />
+      },
+      {
+        path: "about",
+        element: <About />
       }
     ]
   }
