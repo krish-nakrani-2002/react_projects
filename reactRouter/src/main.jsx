d79c3c771c4581c9c9fdc60e9f9e469a3411a828
krish-nakrani-2002 <nakranikrish5@gmail.com>
2024-12-17 11:54:40 +0530

    user component

--- reactRouter/src/main.jsx
+++ reactRouter/src/main.jsx
@@ -2,14 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './Components/Home/home.jsx'
 import About from './Components/About/About.jsx'
 import Contact from './Components/contact/contact.jsx'
+import User from './User/User.jsx'
 
-const router = createBrowserRouter([
-  {
+//const router = createBrowserRouter([
+  /*{
     path: '/',
     element: <Layout/>,
     children: [
@@ -26,8 +27,19 @@ const router = createBrowserRouter([
         element: <Contact />
       }
     ]
-  }
-])
+  }*/
+//])
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='Contact' element={<Contact />} />
+      <Route path='user/:userid' element={<User />} />
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
